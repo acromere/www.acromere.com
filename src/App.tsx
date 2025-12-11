651590e608be44910ba9bc2fd59d4e90231410d0
Mark Soderquist <msoderquist@mastercontrol.com>
2025-12-11 15:33:19 -0700

    Initial web application

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import './App.css';
+
+export function App() {
+  return (
+    <>
+      <h1>www.acromere.com</h1>
+      <p>We're under construction. Please check back for an update soon.</p>
+    </>
+  );
+}
