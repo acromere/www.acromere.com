ed9eb906b99e354b8329bc4645067179151e92e8
Mark Soderquist <mark@soderquist.net>
2026-01-06 19:31:00 -0700

    Add Acromere logo to the landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import './App.css';
+import logo from './image/acromere.png';
 
 export function App() {
   return (
     <>
-      <h1>www.acromere.com</h1>
+      <h1>
+          <img src={logo} alt="" width="64" height="64"/>
+          www.acromere.com
+      </h1>
       <p>We're under construction. Please check back for an update soon.</p>
     </>
   );
