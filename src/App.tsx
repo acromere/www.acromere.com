c73f81c7e0b87c7c432a45dbd4dd5094ded42a67
Mark Soderquist <msoderquist@mastercontrol.com>
2026-01-08 13:28:34 -0700

    Fix formatting

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,17 @@ import './App.css';
 import {Icon} from "./Icon.tsx";
 
 export function App() {
-    return (
-        <>
-            <h1>
-                <picture>
-                    <source srcSet={Icon.LOGO_LIGHT} media="(prefers-color-scheme: light)"/>
-                    <source srcSet={Icon.LOGO_DARK} media="(prefers-color-scheme: dark)"/>
-                    <img src="." alt="ACM" width="64" height="64"/>
-                </picture>
-                &nbsp;Acromere
-            </h1>
-            <p>We're under construction. Please check back for an update soon.</p>
-        </>
-    );
+  return (
+    <>
+      <h1>
+        <picture>
+          <source srcSet={Icon.LOGO_LIGHT} media="(prefers-color-scheme: light)"/>
+          <source srcSet={Icon.LOGO_DARK} media="(prefers-color-scheme: dark)"/>
+          <img src="." alt="ACM" width="64" height="64"/>
+        </picture>
+        &nbsp;Acromere
+      </h1>
+      <p>We're under construction. Please check back for an update soon.</p>
+    </>
+  );
 }
