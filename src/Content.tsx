59bf723771b2e3a322edc62ecee8ca243f3adf69
Mark Soderquist <msoderquist@mastercontrol.com>
2026-01-14 07:34:42 -0700

    Start work on content router

--- src/Content.tsx
+++ src/Content.tsx
@@ -1,7 +1,19 @@
+import React from 'react';
+import {BrowserRouter as Router, Route, Routes} from 'react-router'
+import NotFound from "./NotFound";
+
 export function Content() {
   return (
-    <>
-      <p>We're under construction. Please check back for an update soon.</p>
-    </>
+    <div className='content'>
+      <p className='page'>We're under construction. Please check back for an update soon.</p>
+      <Router>
+        <Routes>
+
+          {/* Default route */}
+          <Route element={<NotFound/>}/>
+        </Routes>
+      </Router>
+
+    </div>
   )
 }
--- src/Icon.tsx
+++ src/Icon.tsx
@@ -1,8 +1,20 @@
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import React from "react";
+
 export class Icon {
   static LOGO_DARK = new URL('./image/acromere-dark.png', import.meta.url);
   static LOGO_LIGHT = new URL('./image/acromere-light.png', import.meta.url);
 
+  static FROWN = <FontAwesomeIcon icon={['fas', 'frown']}/>;
+
   static LOGO = (
+    <picture>
+      <source srcSet={Icon.LOGO_LIGHT.toString()} media="(prefers-color-scheme: light)"/>
+      <source srcSet={Icon.LOGO_DARK.toString()} media="(prefers-color-scheme: dark)"/>
+      <img src="." alt="ACM"/>
+    </picture>
+  )
+  static XENON = (
     <picture>
       <source srcSet={Icon.LOGO_LIGHT.toString()} media="(prefers-color-scheme: light)"/>
       <source srcSet={Icon.LOGO_DARK.toString()} media="(prefers-color-scheme: dark)"/>
