00124a6b4d609764a432ea25ab522eafd609ab77
Mark Soderquist <msoderquist@mastercontrol.com>
2026-01-14 11:04:35 -0700

    Get Font Awesome icons working

--- src/Icon.tsx
+++ src/Icon.tsx
@@ -1,11 +1,21 @@
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import React from "react";
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { library as FontAwesomeLibrary } from '@fortawesome/fontawesome-svg-core'
+
+/* import all the icons in Free Solid, Free Regular, and Brands styles */
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+
+FontAwesomeLibrary.add(fas, far, fab)
 
 export class Icon {
   static LOGO_DARK = new URL('./image/acromere-dark.png', import.meta.url);
   static LOGO_LIGHT = new URL('./image/acromere-light.png', import.meta.url);
 
   static FROWN = <FontAwesomeIcon icon={['fas', 'frown']}/>;
+  // static FROWN = <FontAwesomeIcon icon="fa-solid fa-frown"/>;
 
   static LOGO = (
     <picture>
