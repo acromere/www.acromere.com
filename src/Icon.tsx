2e41b9305a7a71116b4e8b8a18fc65738f13af15
Mark Soderquist <msoderquist@mastercontrol.com>
2026-01-29 11:12:57 -0700

    Add missing icons

--- src/Icon.tsx
+++ src/Icon.tsx
@@ -14,12 +14,24 @@ export default class Icon {
   static XENON_LIGHT = new URL('./image/xenon-light.png', import.meta.url);
   static EMPTY_PIXEL = new URL('./image/empty-pixel.png', import.meta.url);
 
+  static ACORN_LIGHT = new URL('./image/acorn.png', import.meta.url);
+  static AVEON_LIGHT = new URL('./image/aveon.png', import.meta.url);
+  static CARTA_LIGHT = new URL('./image/carta.png', import.meta.url);
+  static MAZER_LIGHT = new URL('./image/mazer.png', import.meta.url);
+  static RECON_LIGHT = new URL('./image/recon.png', import.meta.url);
+  static WEAVE_LIGHT = new URL('./image/weave.png', import.meta.url);
+
   // Needs to be defined before other icons
   static BROKEN = <FontAwesomeIcon className='icon broken' icon={['fas', 'link-slash']}/>;
 
   /* Icon Definitions (Alphabetical order) */
 
-  static ACORN = Icon.BROKEN;
+  static ACORN = (
+    <picture>
+      <source srcSet={Icon.ACORN_LIGHT.toString()} media="(prefers-color-scheme: dark)"/>
+      <img src="." className='icon' alt="ACM"/>
+    </picture>
+  )
   static ACROMERE = (
     <picture>
       <source srcSet={Icon.ACROMERE_LIGHT.toString()} media="(prefers-color-scheme: light)"/>
@@ -27,19 +39,44 @@ export default class Icon {
       <img src="." className='icon' alt="ACM"/>
     </picture>
   )
-  static AVEON = Icon.BROKEN;
-  static CARTA = Icon.BROKEN;
-  static CAMEO = Icon.BROKEN;
-  static CUREX = Icon.BROKEN;
-  static CURVE = Icon.BROKEN;
+  static AVEON = (
+    <picture>
+      <source srcSet={Icon.AVEON_LIGHT.toString()} media="(prefers-color-scheme: dark)"/>
+      <img src="." className='icon' alt="ACM"/>
+    </picture>
+  )
+  static CARTA = (
+    <picture>
+      <source srcSet={Icon.CARTA_LIGHT.toString()} media="(prefers-color-scheme: dark)"/>
+      <img src="." className='icon' alt="ACM"/>
+    </picture>
+  )
+  static CAMEO = Icon.ACROMERE;
+  static CUREX = Icon.ACROMERE;
+  static CURVE = Icon.ACROMERE;
   static EMPTY = <img src={Icon.EMPTY_PIXEL.toString()} className='icon' alt=''/>
   static FROWN = <FontAwesomeIcon className='icon' icon={['fas', 'frown']}/>;
   static LOGO = Icon.ACROMERE;
-  static MAZER = Icon.BROKEN;
+  static MAZER = (
+    <picture>
+      <source srcSet={Icon.MAZER_LIGHT.toString()} media="(prefers-color-scheme: dark)"/>
+      <img src="." className='icon' alt="ACM"/>
+    </picture>
+  )
   static OTHER = <FontAwesomeIcon className='icon' icon={['fas', 'ellipsis']}/>;
-  static RECON = Icon.BROKEN;
-  static SEENC = Icon.BROKEN;
-  static WEAVE = Icon.BROKEN;
+  static RECON = (
+    <picture>
+      <source srcSet={Icon.RECON_LIGHT.toString()} media="(prefers-color-scheme: dark)"/>
+      <img src="." className='icon' alt="ACM"/>
+    </picture>
+  )
+  static SEENC = Icon.ACROMERE;
+  static WEAVE = (
+    <picture>
+      <source srcSet={Icon.WEAVE_LIGHT.toString()} media="(prefers-color-scheme: dark)"/>
+      <img src="." className='icon' alt="ACM"/>
+    </picture>
+  )
   static XENON = (
     <picture>
       <source srcSet={Icon.XENON_LIGHT.toString()} media="(prefers-color-scheme: light)"/>
@@ -47,8 +84,8 @@ export default class Icon {
       <img src="." className='icon' alt="ACM"/>
     </picture>
   )
-  static ZARRA = Icon.BROKEN;
-  static ZENNA = Icon.BROKEN;
-  static ZERRA = Icon.BROKEN;
-  static ZEVRA = Icon.BROKEN;
+  static ZARRA = Icon.ACROMERE;
+  static ZENNA = Icon.ACROMERE;
+  static ZERRA = Icon.ACROMERE;
+  static ZEVRA = Icon.ACROMERE;
 }
