975b44dacb4c9a1545b5ed356f0f364a0bfc80b9
Mark Soderquist <msoderquist@mastercontrol.com>
2026-02-17 12:30:40 -0700

    Light and dark themed product icons

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,23 @@ import {App} from './App';
 
 let container = document.getElementById("app")!;
 let root = createRoot(container)
+
+const lightSchemeIcon = document.querySelector('link#light-scheme-favicon');
+const darkSchemeIcon = document.querySelector('link#dark-scheme-favicon');
+
+let matcher: MediaQueryList = window.matchMedia('(prefers-color-scheme: dark)');
+function updateFaviconScheme() {
+  if (matcher.matches) {
+    if (lightSchemeIcon != null) lightSchemeIcon.remove();
+    if (darkSchemeIcon != null) document.head.append(darkSchemeIcon);
+  } else {
+    if (darkSchemeIcon != null) darkSchemeIcon.remove();
+    if (lightSchemeIcon != null) document.head.append(lightSchemeIcon);
+  }
+}
+matcher.addEventListener('change', updateFaviconScheme);
+updateFaviconScheme();
+
 root.render(
   <StrictMode>
     <App/>
