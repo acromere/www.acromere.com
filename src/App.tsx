31dfa6486d27fd83a161093fac32a631b694deb2
Mark Soderquist <msoderquist@mastercontrol.com>
2026-02-24 16:40:26 -0700

    Cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import './css/app.css';
 import {Header} from './Header.tsx';
 import {Content} from './Content.tsx';
 import {Footer} from './Footer.tsx';
-import {StrictMode} from "react";
 
 export function App() {
   return (
