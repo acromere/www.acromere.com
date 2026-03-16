0ef0b8cd3a9738dd38243fabd8904999e3ee55cb
Mark Soderquist <msoderquist@mastercontrol.com>
2026-03-16 09:22:36 -0600

    Use exported member App

--- client/index.tsx
+++ client/index.tsx
@@ -1,6 +1,6 @@
 import {createRoot} from 'react-dom/client';
 import {StrictMode} from 'react';
-import {App} from './App';
+import App from './App';
 
 let container = document.getElementById("root")!;
 let root = createRoot(container)
